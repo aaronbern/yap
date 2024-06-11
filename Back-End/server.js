94cc197bb1d6b37bc899e32cc973ac88b00de005
Aaron.Bernard <aaronber@pdx.edu>
2024-06-11 01:51:24 -0700

    Update directory name to front-end

--- Back-End/server.js
+++ Back-End/server.js
@@ -99,9 +99,9 @@ app.post('/upload', (req, res) => {
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '../front-end/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../front-end', 'build', 'index.html'));
     });
 } else {
     app.get('/', (req, res) => {
